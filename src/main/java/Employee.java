b397abb0fb27922b442e531d249b2e82a77df779
dustinquintanilla <portillo0121@gmail.com>
2024-10-01 09:05:05 -0400

    Created employee object class with comments explaining methods

--- src/main/java/Employee.java
+++ src/main/java/Employee.java
@@ -1,13 +1,59 @@
+//This class handles Employee object creation.
+//This lets the program keep track of which employee is which,
+
 public class Employee {
 
-    private String id;
-    private String firstName;
-    private String lastName;
+    private final String ID;
+    private final String firstName;
+    private final String lastName;
+    private final String position;
+    private boolean clockedIn;
 
-    public Employee() {
+    //Creates employee object and saves their ID, first name, last name, and position
+    //Sets their clockedIn variable to false, in case this employee did not mean to log in
+    public Employee(String ID, String firstName, String lastName, String position) {
+        this.ID = ID;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.position = position;
+        clockedIn = false;
+    }
+
+    //Method to return employee ID
+    public String getID() {
+        return ID;
+    }
+
+    //Set of methods to return employee names
+    public String getFullName() {
+        return firstName + " " + lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    //Method to get employee position
+    public String getPosition() {
+        return position;
+    }
 
 
+    //Set of methods to return and set clockedIn status
+    public boolean clockedIn() {
+        return clockedIn;
+    }
 
+    public void clockIn() {
+        clockedIn = true;
+    }
+
+    public void clockOut() {
+        clockedIn = false;
     }
 
 }
