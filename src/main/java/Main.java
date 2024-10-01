36bc76f2ff4860997a2b05f04812dffea190aca5
dustinquintanilla <portillo0121@gmail.com>
2024-10-01 15:00:17 -0400

    added database to create sqlite database for records keeping

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,18 @@
+import java.io.File;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        File dburl = new File("register_database.sqlite");
 
+        if (!dburl.exists()) {
+            new createDatabase();
+        }
+
+        else {
+
+        }
 
     }
 
