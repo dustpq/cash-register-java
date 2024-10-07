388f95b24461eb0a0871c4bfabdc6cb3fb4c15a6
dustinquintanilla <portillo0121@gmail.com>
2024-10-07 17:54:50 -0400

    renamed Main to MainApp for clarity

---
+++ src/main/java/MainApp.java
@@ -0,0 +1,22 @@
+import java.io.File;
+
+public class MainApp {
+
+    static String db_url = "jdbc:sqlite:register_database.sqlite";
+
+    public static void main(String[] args) {
+
+        File db_File = new File("register_database.sqlite");
+
+        if (!db_File.exists()) {
+            new createDatabase();
+        }
+
+        else {
+            System.out.println("Database present. Proceeding...");
+            new loginScreen();
+        }
+
+    }
+
+}
