e58f801e0dc6d299c7decfbc2e458b5d5209191b
dustinquintanilla <portillo0121@gmail.com>
2024-11-06 17:42:30 -0500

    created log in screen using swing ui designer that uses the logInManager class to handle logging in and invalid ids

---
+++ src/main/java/loginScreenSUI.java
@@ -0,0 +1,95 @@
+import javax.swing.*;
+
+public class loginScreenSUI {
+
+    private JPanel panel;
+    private JLabel questionLabel;
+    private JTextField idDisplay;
+    private JButton numkey1;
+    private JButton numkey2;
+    private JButton numkey3;
+    private JButton numkey4;
+    private JButton numkey5;
+    private JButton numkey6;
+    private JButton numkey7;
+    private JButton numkey8;
+    private JButton numkey9;
+    private JButton numkey0;
+    private JButton keyClear;
+    private JButton keyOK;
+    private String idInput = "EMP";
+
+    public loginScreenSUI() {
+
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setContentPane(panel);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+
+        idDisplay.setText(idInput);
+
+        numkey1.addActionListener(e -> addNumToString(1));
+        numkey2.addActionListener(e -> addNumToString(2));
+        numkey3.addActionListener(e -> addNumToString(3));
+        numkey4.addActionListener(e -> addNumToString(4));
+        numkey5.addActionListener(e -> addNumToString(5));
+        numkey6.addActionListener(e -> addNumToString(6));
+        numkey7.addActionListener(e -> addNumToString(7));
+        numkey8.addActionListener(e -> addNumToString(8));
+        numkey9.addActionListener(e -> addNumToString(9));
+        numkey0.addActionListener(e -> addNumToString(0));
+
+        keyClear.addActionListener(e -> clear_idDisplay());
+        keyOK.addActionListener(e -> {
+            idInput = idDisplay.getText();
+            tryLogin(idInput);
+        });
+
+    }
+
+    private void addNumToString(int num) {
+
+        if (idInput.length()<12) {
+
+            idInput += num;
+            idDisplay.setText(idInput);
+
+        }
+
+    }
+
+    private void clear_idDisplay() {
+
+        idInput = "EMP";
+        idDisplay.setText(idInput);
+
+    }
+
+    private void tryLogin(String userID) {
+
+        System.out.println("Trying to login with ID: " + userID);
+
+        if (loginManager.logIn(userID)) {
+            System.out.println("Login successful.");
+        }
+
+        else {
+            System.out.println("Login failed.");
+            logInFail();
+        }
+
+    }
+
+    private void logInFail() {
+
+        JOptionPane.showMessageDialog(
+                null,
+                "Login failed. Try again.",
+                "Login Error",
+                JOptionPane.ERROR_MESSAGE);
+
+    }
+
+}
