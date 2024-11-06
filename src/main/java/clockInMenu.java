a2c9f6ed662c751b581d050d049d485b723559aa
dustinquintanilla <portillo0121@gmail.com>
2024-11-06 17:37:32 -0500

    placeholder class for clock in menu

---
+++ src/main/java/clockInMenu.java
@@ -0,0 +1,12 @@
+public class clockInMenu {
+
+    private Employee e;
+
+    public clockInMenu(Employee e) {
+
+        System.out.println("Clock in menu started.");
+        this.e = e;
+
+    }
+
+}
