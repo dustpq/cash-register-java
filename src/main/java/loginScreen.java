4b2eb98615b38cd87c033d9c4ecdea8f5877bbcd
dustinquintanilla <portillo0121@gmail.com>
2024-11-06 17:44:00 -0500

    started creating a login manually, but used the ui designer in intellij, keeping for possible usage.

--- src/main/java/loginScreen.java
+++ src/main/java/loginScreen.java
@@ -5,17 +5,28 @@ public class loginScreen {
 
     public loginScreen() {
 
-        JFrame frame = new JFrame();
-        frame.setSize(320,520);
-        frame.setBounds(10,10,10,10);
-        frame.setLayout(new GridBagLayout());
+        JFrame frame = new JFrame("Employee Log In");
+        frame.setSize(520,320);
         frame.setVisible(true);
         frame.setLocationRelativeTo(null);
-        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridBagLayout());
+        frame.add(panel);
+        panel.setVisible(true);
 
         GridBagConstraints c = new GridBagConstraints();
+        c.insets = new Insets(10, 10, 10, 10);
+        c.anchor = GridBagConstraints.CENTER;
+        c.gridwidth = 1;
+        c.fill = GridBagConstraints.NONE;
 
-        c.gridx = 1;
+        JLabel questionLabel = new JLabel("Enter employee ID: ");
+        c.gridx = 2;
+        c.gridy = 0;
+        c.fill = GridBagConstraints.HORIZONTAL;
+        panel.add(questionLabel, c);
 
 
     }
