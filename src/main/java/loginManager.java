3ed85e98beb3b8f17e2c6d6713dab1c56e025546
dustinquintanilla <portillo0121@gmail.com>
2024-12-04 18:31:11 -0500

    seperated the login method into an actual login method and a loggedin checker method

--- src/main/java/loginManager.java
+++ src/main/java/loginManager.java
@@ -1,18 +1,15 @@
-import javax.swing.*;
 import java.util.ArrayList;
 
 public class loginManager {
 
     static ArrayList<Employee> loggedInEmployees = new ArrayList<>();
 
-    public static boolean logIn(String id) {
+    public static void logIn(String id) {
 
         if (employeeTable.isValidID(id)) {
 
             System.out.println("ID is valid, proceeding with login check...");
 
-            // Creating newEmployee to check if it exists,
-            // and if it doesn't, it will be added to the list of logged in employees
             Employee newEmployee = new Employee(
                     id,
                     employeeTable.getFirstNameFromID(id),
@@ -21,41 +18,39 @@ public class loginManager {
 
             for (Employee employee : loggedInEmployees) {
 
-                // If the employee is already logged in
                 if (employee.getID().equals(newEmployee.getID())) {
 
                     System.out.println("Employee is logged in.");
 
                     if (employee.clockedIn()) {
-
                         System.out.println("Employee is clocked in, proceeding to main menu...");
                         new mainMenu(employee);
-
                     } else {
-
                         System.out.println("Employee is not clocked in, proceeding to clock in menu...");
                         new clockInMenu(employee);
-
                     }
 
-                    return true;
+                    return;
                 }
             }
 
-            // If the employee is not already logged in
             System.out.println("Employee not logged in, logging in now...");
             loggedInEmployees.add(newEmployee);
 
             System.out.println("Employee is not clocked in, proceeding to clock in menu...");
             new clockInMenu(newEmployee);
 
-            return true;
-
         } else {
             System.out.println("ERROR: ID is not valid.");
-            return false;
         }
-
     }
 
+    public static boolean isLoggedIn(String id) {
+        for (Employee employee : loggedInEmployees) {
+            if (employee.getID().equals(id)) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
