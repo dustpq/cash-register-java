983ef011b5105a618c7b611b4c1ef6095fd742a9
dustinquintanilla <portillo0121@gmail.com>
2024-12-09 14:59:33 -0500

    work done to further the menu

--- src/main/java/mainMenu.java
+++ src/main/java/mainMenu.java
@@ -1,11 +1,46 @@
+import javax.swing.*;
+
 public class mainMenu {
 
-    private Employee e;
+    private Employee employee;
+    private JPanel panel;
+    private JButton exitButton;
+    private JButton delete_button;
+    private JButton functionsButton;
+    private JButton button4;
+    private JButton prevButton;
+    private JButton nextButton;
+    private JButton newTicketButton;
+    private JButton quantityButton;
+    private JButton button1;
+    private JButton button5;
+    private JButton button6;
+    private JButton button9;
+    private JButton button10;
+    private JButton button11;
+    private JLabel name_label;
+    private JLabel position_label;
+    private JLabel time_label;
+    private JPanel menuPanel;
+    private JButton button3;
+    private JButton button12;
+    private JButton button13;
+    private JButton button14;
+    private JLabel ticket_num;
+    private JPanel food_menu;
+    private JPanel transaction_view;
+    private JButton burgerButton;
+    private JButton wingsButton;
+    private JButton button8;
+    private JButton button2;
+    private JButton button7;
 
-    public mainMenu(Employee e) {
+    public mainMenu(Employee employee) {
 
         System.out.println("Main menu started.");
-        this.e = e;
+        this.employee = employee;
+
+
 
     }
 
