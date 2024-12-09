e0a238da156a13852b1bcc12e225edc0a3ea11bf
dustinquintanilla <portillo0121@gmail.com>
2024-12-09 14:59:14 -0500

    simple menu created for demo

---
+++ src/main/java/simpleMenu.java
@@ -0,0 +1,95 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class simpleMenu {
+
+    private Employee employee;
+    private JPanel mainPanel;
+    private JPanel transactionView;
+    private JPanel foodView;
+    private JList itemList;
+    private JButton exitButton;
+    private JButton deleteItemButton;
+    private JButton burgerButton;
+    private JButton hotDogButton;
+    private JLabel timeLabel;
+    private JLabel nameLabel;
+    private JLabel positionLabel;
+    private JPanel burgerMenu;
+    private JPanel hotdogMenu;
+    private JButton extraPattyButton;
+    private JButton lettuceButton;
+    private JButton onionsButton;
+    private JButton picklesButton;
+    private JButton tomatoButton;
+    private JButton bketchupButton;
+    private JButton bmustardButton;
+    private JButton sausageUpgradeButton;
+    private JButton ketchupButton1;
+    private JButton mustardButton1;
+    private JButton mayoButton;
+    private JButton relishButton;
+    private JButton onionsButton1;
+    private JButton picklesButton1;
+    private Timer timer;
+    private DefaultListModel<Item> listModel;
+
+
+    public simpleMenu(Employee employee) {
+
+        this.employee = employee;
+
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setContentPane(mainPanel);
+        frame.setSize(600,600);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+
+        nameLabel.setText(employee.getFullName());
+        positionLabel.setText(employee.getPosition());
+
+        timeLabel.setText("--:--:-- --");
+        startClock();
+
+        listModel = new DefaultListModel<>();
+        itemList.setModel(listModel);
+
+
+        //burger button changes to burger menu
+        burgerButton.addActionListener(e -> {
+            CardLayout cl = (CardLayout) (foodView.getLayout());
+            cl.show(foodView, "burgerMenu");
+            Item burger = new Item("Burger");
+            listModel.addElement(burger);
+        });
+
+        //hot dog button changes to hot dog menu
+        hotDogButton.addActionListener(e -> {
+            CardLayout cl = (CardLayout) (foodView.getLayout());
+            cl.show(foodView, "hotdogMenu");
+        });
+
+        exitButton.addActionListener(e -> {
+            new loginScreenSUI();
+            frame.dispose();
+        });
+
+    }
+
+    private void startClock() {
+        timer = new Timer(1000, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                SimpleDateFormat sdf = new SimpleDateFormat("hh:mm:ss a");
+                timeLabel.setText(sdf.format(new Date()));
+            }
+        });
+        timer.start();
+    }
+
+}
